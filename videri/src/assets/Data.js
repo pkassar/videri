b3f8555f60e27c166543fef6395758f3d83a83d0
pkassar <paulkassar@gmail.com>
2018-06-25 21:29:41 -0400

    data.js added

---
+++ videri/src/assets/Data.js
@@ -0,0 +1,109 @@
+var DATA = {
+  folders: [{
+      "name": "extreme",
+      "createdAt": "2017/08/04",
+      "coverPic": "https://s8.postimg.cc/dcwimei0l/Screen_Shot_2018-06-24_at_17.03.01.png",
+      "assets": [{
+        "title": "vehicle gopro hd.mp4",
+        "fileCover": "https://s8.postimg.cc/cqza1kt7p/Screen_Shot_2018-06-25_at_11.37.24.png",
+        "length": "00:30",
+        "created": "2017/02/13 06:34"
+      }, {
+        "title": "Surf wave.mp4",
+        "fileCover": "https://s8.postimg.cc/72szaoh5h/Screen_Shot_2018-06-25_at_11.37.28.png",
+        "length": "00:12",
+        "created": "2017/05/03 21:23"
+      }, {
+        "title": "car chase.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:28",
+        "created": "2017/12/11 13:34"
+      }]
+    }, {
+      "name": "extreme2",
+      "createdAt": "2017/08/04",
+      "coverPic": "https://s8.postimg.cc/ykatnwlmt/Screen_Shot_2018-06-25_at_15.24.22.png",
+      "assets": [{
+        "title": "vehicle gopro hd.mp4",
+        "fileCover": "https://s8.postimg.cc/cqza1kt7p/Screen_Shot_2018-06-25_at_11.37.24.png",
+        "length": "00:30",
+        "created": "2017/02/13 06:34"
+      }, {
+        "title": "Surf wave.mp4",
+        "fileCover": "https://s8.postimg.cc/72szaoh5h/Screen_Shot_2018-06-25_at_11.37.28.png",
+        "length": "00:12",
+        "created": "2017/05/03 21:23"
+      }, {
+        "title": "car chase.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:28",
+        "created": "2017/12/11 13:34"
+      }]
+    },
+    {
+      "name": "4Kcontent",
+      "createdAt": "2017/06/11",
+      "coverPic": "https://s8.postimg.cc/7oq7vi5yd/Screen_Shot_2018-06-24_at_17.03.20.png",
+      "assets": [{
+        "title": "vehicle gopro hd.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:30",
+        "created": "2017/02/13 06:34"
+      }, {
+        "title": "Surf wave.mp4",
+        "fileCover": "https://s8.postimg.cc/ndt36zlxh/Screen_Shot_2018-06-25_at_11.37.58.png",
+        "length": "00:12",
+        "created": "2017/05/03 21:23"
+      }, {
+        "title": "car chase.mp4",
+        "fileCover": "https://s8.postimg.cc/cqza1kt7p/Screen_Shot_2018-06-25_at_11.37.24.png",
+        "length": "00:28",
+        "created": "2017/12/11 13:34"
+      }]
+    },
+    {
+      "name": "Sports",
+      "createdAt": "2017/12/12",
+      "coverPic": "https://s8.postimg.cc/f4phhb939/Screen_Shot_2018-06-24_at_17.03.05.png",
+      "assets": [{
+        "title": "vehicle gopro hd.mp4",
+        "fileCover": "https://s8.postimg.cc/ndt36zlxh/Screen_Shot_2018-06-25_at_11.37.58.png",
+        "length": "00:30",
+        "created": "2017/02/13 06:34"
+      }, {
+        "title": "Surf wave.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:12",
+        "created": "2017/05/03 21:23"
+      }, {
+        "title": "car chase.mp4",
+        "fileCover": "https://s8.postimg.cc/ndt36zlxh/Screen_Shot_2018-06-25_at_11.37.58.png",
+        "length": "00:28",
+        "created": "2017/12/11 13:34"
+      }]
+    },
+    {
+      "name": "Product",
+      "createdAt": "2018/01/02",
+      "coverPic": "https://s8.postimg.cc/81hm1pbdh/Screen_Shot_2018-06-24_at_17.03.11.png",
+      "assets": [{
+        "title": "vehicle gopro hd.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:30",
+        "created": "2017/02/13 06:34"
+      }, {
+        "title": "Surf wave.mp4",
+        "fileCover": "https://s8.postimg.cc/7sbrn1x4l/Screen_Shot_2018-06-25_at_11.37.31.png",
+        "length": "00:12",
+        "created": "2017/05/03 21:23"
+      }, {
+        "title": "car chase.mp4",
+        "fileCover": "https://s8.postimg.cc/cqza1kt7p/Screen_Shot_2018-06-25_at_11.37.24.png",
+        "length": "00:28",
+        "created": "2017/12/11 13:34"
+      }]
+    }
+  ]
+}
+
+export default DATA
